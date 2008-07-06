458b6b784f4431fc3d3eee4e0de9107251102450
kzu.net <kzu.net@b33fba48-7441-0410-8d5c-f397f7ceaa6c>
2008-07-06 23:36:40 +0000

    Fixed license text on files. Removed Contrib project which will now live in its own moq-contrib project.
    git-svn-id: http://moq.googlecode.com/svn/trunk@206 b33fba48-7441-0410-8d5c-f397f7ceaa6c

--- Source/Range.cs
+++ Source/Range.cs
@@ -15,7 +15,7 @@
 //    and the following disclaimer in the documentation 
 //    and/or other materials provided with the distribution.
     
-//    * Neither the name of the <ORGANIZATION> nor the 
+//    * Neither the name of the Moq Team nor the 
 //    names of its contributors may be used to endorse 
 //    or promote products derived from this software 
 //    without specific prior written permission.
