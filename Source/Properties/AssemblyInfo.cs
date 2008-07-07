23623dab61ccda0acc451345858783cb86ef08f1
kzu.net <kzu.net@b33fba48-7441-0410-8d5c-f397f7ceaa6c>
2008-07-07 03:04:34 +0000

    Updated version information for release 2.6
    git-svn-id: http://moq.googlecode.com/svn/trunk@211 b33fba48-7441-0410-8d5c-f397f7ceaa6c

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -1,14 +1,14 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 
-[assembly: AssemblyTitle("MoQ")]
+[assembly: AssemblyTitle("Moq")]
 [assembly: AssemblyDescription("Mocking library for .NET 3.5 and C# 3.0, heavily based on Linq")]
 [assembly: AssemblyCompany("Clarius Consulting, Manas Technology Solutions, InSTEDD")]
-[assembly: AssemblyProduct("MoQ")]
+[assembly: AssemblyProduct("Moq")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("2.5.*")]
-[assembly: AssemblyFileVersion("2.5.0.0")]
+[assembly: AssemblyVersion("2.6.*")]
+[assembly: AssemblyFileVersion("2.6.0.0")]
 
 [assembly: InternalsVisibleTo("Moq.Tests, PublicKey=00240000048000009400000006020000002400005253413100040000010001009f7a95086500f8f66d892174803850fed9c22225c2ccfff21f39c8af8abfa5415b1664efd0d8e0a6f7f2513b1c11659bd84723dc7900c3d481b833a73a2bcf1ed94c16c4be64d54352c86956c89930444e9ac15124d3693e3f029818e8410f167399d6b995324b635e95353ba97bfab856abbaeb9b40c9b160070c6325e22ddc")]
 
