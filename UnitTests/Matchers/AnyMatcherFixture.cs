7711d92984b8610f6257d0b6597fc1cfd8c90f75
kzu.net <kzu.net@b33fba48-7441-0410-8d5c-f397f7ceaa6c>
2008-09-19 00:25:50 +0000

    Added tests for fix for Issue 86:  	 IsAny<T> should check if the value is actually of type T
    git-svn-id: http://moq.googlecode.com/svn/trunk@226 b33fba48-7441-0410-8d5c-f397f7ceaa6c

---
+++ UnitTests/Matchers/AnyMatcherFixture.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using System.Linq.Expressions;
+
+namespace Moq.Tests.Matchers
+{
+	public class AnyMatcherFixture
+	{
+		[Fact]
+		public void MatchesNull()
+		{
+			var matcher = new AnyMatcher();
+
+			Assert.True(matcher.Matches(null));
+		}
+
+		[Fact]
+		public void MatchesIfAssignableType()
+		{
+			var expr = ToExpression<object>(() => It.IsAny<object>()).ToLambda().Body;
+
+			var matcher = MatcherFactory.CreateMatcher(expr);
+			matcher.Initialize(expr);
+
+			Assert.True(matcher.Matches("foo"));
+		}
+
+		[Fact]
+		public void DoesntMatchIfNotAssignableType()
+		{
+			var expr = ToExpression<IFormatProvider>(() => It.IsAny<IFormatProvider>()).ToLambda().Body;
+
+			var matcher = MatcherFactory.CreateMatcher(expr);
+			matcher.Initialize(expr);
+
+			Assert.False(matcher.Matches("foo"));
+		}
+
+		private Expression ToExpression<TResult>(Expression<Func<TResult>> expr)
+		{
+			return expr;
+		}
+	}
+}
