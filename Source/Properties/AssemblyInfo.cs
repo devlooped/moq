24ea6acff42b5b70e38a84b3a9ef42a56d57e585
kzu.net <kzu.net@b33fba48-7441-0410-8d5c-f397f7ceaa6c>
2009-02-22 13:31:53 +0000

    CI: updated AssemblyInfo version to 3.0.222.1
    git-svn-id: http://moq.googlecode.com/svn/trunk@491 b33fba48-7441-0410-8d5c-f397f7ceaa6c

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("Moq")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("3.0.204.1")]
+[assembly: AssemblyVersion("3.0.222.1")]
 [assembly: AssemblyFileVersion("3.0.0.0")]
 
 [assembly: ComVisible(false)]
