2a4efea6332d9b0f851da205c8c9bf433b6c2a97
kzu.net <kzu.net@b33fba48-7441-0410-8d5c-f397f7ceaa6c>
2009-03-12 13:31:14 +0000

    Fixed #154
    git-svn-id: http://moq.googlecode.com/svn/trunk@549 b33fba48-7441-0410-8d5c-f397f7ceaa6c

---
+++ Source/Sub/StubExtensions.cs
@@ -0,0 +1,90 @@
+﻿//Copyright (c) 2007, Moq Team 
+//http://code.google.com/p/moq/
+//All rights reserved.
+
+//Redistribution and use in source and binary forms, 
+//with or without modification, are permitted provided 
+//that the following conditions are met:
+
+//    * Redistributions of source code must retain the 
+//    above copyright notice, this list of conditions and 
+//    the following disclaimer.
+
+//    * Redistributions in binary form must reproduce 
+//    the above copyright notice, this list of conditions 
+//    and the following disclaimer in the documentation 
+//    and/or other materials provided with the distribution.
+
+//    * Neither the name of the Moq Team nor the 
+//    names of its contributors may be used to endorse 
+//    or promote products derived from this software 
+//    without specific prior written permission.
+
+//THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND 
+//CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
+//INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
+//MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+//DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR 
+//CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+//SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+//BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+//SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+//INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+//WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
+//NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+//OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF 
+//SUCH DAMAGE.
+
+//[This is the BSD license, see
+// http://www.opensource.org/licenses/bsd-license.php]
+
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.ComponentModel;
+
+namespace Moq.Stub
+{
+	/// <summary>
+	/// Legacy Stub stuff, moved to the core API.
+	/// </summary>
+	[EditorBrowsable(EditorBrowsableState.Never)]
+	public static class StubExtensions
+	{
+		/// <summary>
+		/// Obsolete. Use <see cref="Mock{T}.SetupProperty{TProperty}(Expression{Func{T, TProperty}})"/>.
+		/// </summary>
+		[Obsolete("Please use mock.SetupProperty instead.", false)]
+		[EditorBrowsable(EditorBrowsableState.Never)]
+		public static void Stub<T, TProperty>(this Mock<T> mock, Expression<Func<T, TProperty>> property)
+			 where T : class
+		{
+			mock.SetupProperty(property);
+		}
+
+		/// <summary>
+		/// Obsolete. Use <see cref="Mock{T}.SetupProperty{TProperty}(Expression{Func{T, TProperty}}, TProperty)"/>.
+		/// </summary>
+		[Obsolete("Please use mock.SetupProperty instead.", false)]
+		[EditorBrowsable(EditorBrowsableState.Never)]
+		public static void Stub<T, TProperty>(this Mock<T> mock, Expression<Func<T, TProperty>> property, TProperty initialValue)
+			 where T : class
+		{
+			mock.SetupProperty(property, initialValue);
+		}
+
+#if !SILVERLIGHT
+		/// <summary>
+		/// Obsolete. Use <see cref="Mock{T}.SetupAllProperties"/>.
+		/// </summary>
+		[Obsolete("Please use mock.SetupProperty instead.", false)]
+		[EditorBrowsable(EditorBrowsableState.Never)]
+		public static void StubAll<T>(this Mock<T> mock)
+			 where T : class
+		{
+			mock.SetupAllProperties();
+		}
+#endif
+	}
+}
