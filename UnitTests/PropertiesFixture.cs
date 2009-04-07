13a1697dab447f190d7138902aec864364be1dd3
kzu.net <kzu.net@b33fba48-7441-0410-8d5c-f397f7ceaa6c>
2009-04-07 22:56:36 +0000

    Started moving fixtures related to properties to a single fixture class.
    git-svn-id: http://moq.googlecode.com/svn/trunk@562 b33fba48-7441-0410-8d5c-f397f7ceaa6c

---
+++ UnitTests/PropertiesFixture.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Moq.Tests
+{
+	public class PropertiesFixture
+	{
+		public interface IIndexedFoo
+		{
+			string this[int key] { get; set; }
+		}
+
+		[Fact]
+		public void ShouldThrowIfSetupSetIndexer()
+		{
+			var foo = new Mock<IIndexedFoo>();
+
+			Assert.Throws<ArgumentException>(() => foo.SetupSet(f => f[0]));
+		}
+
+		[Fact]
+		public void ShouldSetIndexer()
+		{
+			var foo = new Mock<IIndexedFoo>(MockBehavior.Strict);
+
+			foo.SetupSet(f => f[0] = "foo");
+
+			foo.Object[0] = "foo";
+		}
+
+		[Fact]
+		public void ShouldSetIndexerWithValueMatcher()
+		{
+			var foo = new Mock<IIndexedFoo>(MockBehavior.Strict);
+
+			foo.SetupSet(f => f[0] = It.IsAny<string>());
+
+			foo.Object[0] = "foo";
+		}
+
+		[Fact(Skip = "Not supported for now")]
+		public void ShouldSetIndexerWithIndexMatcher()
+		{
+			var foo = new Mock<IIndexedFoo>(MockBehavior.Strict);
+
+			foo.SetupSet(f => f[It.IsAny<int>()] = "foo");
+
+			foo.Object[18] = "foo";
+		}
+
+		[Fact(Skip = "Not supported for now")]
+		public void ShouldSetIndexerWithBothMatcher()
+		{
+			var foo = new Mock<IIndexedFoo>(MockBehavior.Strict);
+
+			foo.SetupSet(f => f[It.IsAny<int>()] = It.IsAny<string>());
+
+			foo.Object[18] = "foo";
+		}
+	}
+}
--- UnitTests/Regressions/IssueReportsFixture.cs
+++ UnitTests/Regressions/IssueReportsFixture.cs
@@ -328,58 +328,7 @@ namespace Moq.Tests.Regressions
 
 		public class _136
 		{
-			public interface IIndexedFoo
-			{
-				string this[int key] { get; set; }
-			}
-
-			[Fact]
-			public void ShouldThrowIfSetupSetIndexer()
-			{
-				var foo = new Mock<IIndexedFoo>();
-
-				Assert.Throws<ArgumentException>(() => foo.SetupSet(f => f[0]));
-			}
-
-			[Fact]
-			public void ShouldSetIndexer()
-			{
-				var foo = new Mock<IIndexedFoo>(MockBehavior.Strict);
-
-				foo.SetupSet(f => f[0] = "foo");
-
-				foo.Object[0] = "foo";
-			}
-
-			[Fact]
-			public void ShouldSetIndexerWithValueMatcher()
-			{
-				var foo = new Mock<IIndexedFoo>(MockBehavior.Strict);
-
-				foo.SetupSet(f => f[0] = It.IsAny<string>());
-
-				foo.Object[0] = "foo";
-			}
-
-			[Fact(Skip = "Not supported")]
-			public void ShouldSetIndexerWithIndexMatcher()
-			{
-				var foo = new Mock<IIndexedFoo>(MockBehavior.Strict);
-
-				foo.SetupSet(f => f[It.IsAny<int>()] = "foo");
-
-				foo.Object[18] = "foo";
-			}
-
-			[Fact(Skip = "Not supported")]
-			public void ShouldSetIndexerWithBothMatcher()
-			{
-				var foo = new Mock<IIndexedFoo>(MockBehavior.Strict);
-
-				foo.SetupSet(f => f[It.IsAny<int>()] = It.IsAny<string>());
-
-				foo.Object[18] = "foo";
-			}
+			// Fixed on PropertiesFixture.cs
 		}
 
 		#endregion
