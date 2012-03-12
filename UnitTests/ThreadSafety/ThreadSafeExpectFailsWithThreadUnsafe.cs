f9a4b5c3bcde6772f984070b48d4b7a5edcf826f
FelicePollano <felice@felicepollano.com>
2012-03-12 10:44:01 +0100

    Mock is now thread safe. Added a unit test to show that now works. Added a unit test commented to show the previous version was not thread safe.

---
+++ UnitTests/ThreadSafety/ThreadSafeExpectFailsWithThreadUnsafe.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using System.Threading;
+
+namespace Moq.Tests.ThreadSafe
+{
+    /// <summary>
+    /// This is a test showing moq fails in multithread
+    /// </summary>
+    public class ThreadSafeExpectFailsWithThreadUnsafe
+    {
+        
+        /*
+        [Fact]
+        public void ExpectFailWithStandardMock()
+        {
+            int concurrent = 8;
+            var v = new Mock<ExpectToBeThreadSafe>();
+            
+            v.CallBase = true;
+            Exception failedWithException = null;
+            try
+            {
+                for (int i = 0; i < concurrent; ++i)
+                {
+                    ThreadPool.QueueUserWorkItem(
+                        (k) =>
+                        {
+                            try
+                            {
+                                v.Object.DoTheJob();
+                            }
+                            catch (Exception inner)
+                            {
+                                failedWithException = inner;
+                            }
+                        }
+                        );
+                }
+                Thread.Sleep(200);
+                if (null == failedWithException)
+                {
+                    v.Verify(k => k.DoTheJob(), Times.Exactly(concurrent));
+                    v.Verify(k => k.RaceCondition(), Times.AtLeastOnce());
+                }
+            }
+            catch (Exception e)
+            {
+                //sometimes thread unsafe fatally fails 
+                //so the test is obviously passed
+                failedWithException = e;
+            }
+        }
+          */
+    }
+}
---
+++ UnitTests/ThreadSafety/ThreadSafeMockDoesNotAllowRaceConditions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using System.Threading;
+
+namespace Moq.Tests.ThreadSafe
+{
+    public class ThreadSafeMockDoesNotAllowRaceConditions
+    {
+       
+        [Fact]
+        public void ExpectAvoidRaceConditions()
+        {
+            int concurrent = 8;
+            var v = new Mock<ExpectToBeThreadSafe>();
+
+            v.CallBase = true;
+            for (int i = 0; i < concurrent; ++i)
+            {
+                ThreadPool.QueueUserWorkItem(
+                    (k) =>
+                    {
+                        v.Object.DoTheJob();
+                    }
+                    );
+            }
+
+            Thread.Sleep(200);
+            v.Verify(k => k.DoTheJob(), Times.Exactly(concurrent));
+            v.Verify(k => k.RaceCondition(), Times.Never());
+        }
+    }
+}
