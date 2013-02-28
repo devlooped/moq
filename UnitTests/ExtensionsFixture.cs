0b9a9a5101d6c25abdce11b41bf3143a3928afda
salfab <fabio.salvalai@gmail.com>
2013-02-28 23:11:20 +0100

    Fix poorly written test (sorry).
    - test was using obsolete methods, leading to a failed build in Release mode, because of the unit test was failing.

--- UnitTests/ExtensionsFixture.cs
+++ UnitTests/ExtensionsFixture.cs
@@ -6,30 +6,37 @@ using Xunit;
 
 namespace Moq.Tests
 {
-	public class ExtensionsFixture
-	{
-		[Fact]
-		public void IsMockeableReturnsFalseForValueType()
-		{
-			Assert.False(typeof(int).IsMockeable());
-		}
+    public class ExtensionsFixture
+    {
+        #region Public Methods
 
-        // [Fact]
-        // public void OnceDoesNotThrowOnSecondCallIfCountWasResetBefore()
-        // {
-        //     var mock = new Mock<IFooReset>();
-        //     mock.Setup(foo => foo.Execute("ping"))
-        //         .Returns("ack")
-        //         .AtMostOnce();
-           
-        //     Assert.Equal("ack", mock.Object.Execute("ping"));
-        //     mock.ResetAllCalls();
-        //     Assert.DoesNotThrow(() => mock.Object.Execute("ping"));
-        // }
-	}
+        [Fact]
+        public void IsMockeableReturnsFalseForValueType()
+        {
+            Assert.False(typeof(int).IsMockeable());
+        }
+
+        [Fact]
+        public void OnceDoesNotThrowOnSecondCallIfCountWasResetBefore()
+        {
+            var mock = new Mock<IFooReset>();
+            mock.Setup(foo => foo.Execute("ping")).Returns("ack");
+
+            mock.Object.Execute("ping");
+            mock.ResetAllCalls();
+            mock.Object.Execute("ping");
+            mock.Verify(o => o.Execute("ping"), Times.Once());
+        }
+
+        #endregion
+    }
 
     public interface IFooReset
     {
+        #region Public Methods
+
         object Execute(string ping);
+
+        #endregion
     }
 }
