84cb112154cad204ab6c88202857b70e95668ac3
grzesiek-galezowski <grzesiek.galezowski@gmail.com>
2014-09-02 23:49:18 +0200

    Fixed formatting of fixtures

--- UnitTests/VerifyInSequenceFixture.cs
+++ UnitTests/VerifyInSequenceFixture.cs
@@ -1,52 +1,52 @@
-﻿using	System;
-using	Moq.Sequencing;
-using	Xunit;
+﻿using System;
+using Moq.Sequencing;
+using Xunit;
 
-namespace	Moq.Tests
+namespace Moq.Tests
 {
-	
-	public interface RoleWithSingleSimplestMethod
-	{
-		void Do();
-	}
 
-	public interface RoleWithArgumentAndReturnValue
-	{
-		string Do(int	a);
-	}
+  public interface RoleWithSingleSimplestMethod
+  {
+    void Do();
+  }
 
-	public interface RoleForRecursiveMocking
-	{
-		RoleWithSingleSimplestMethod Do();
-	}
+  public interface RoleWithArgumentAndReturnValue
+  {
+    string Do(int a);
+  }
 
-	public interface RoleWithProperty
-	{
-		string Anything	{	get; set;	}
-		string AnythingElse	{	get; set;	}
-	}
+  public interface RoleForRecursiveMocking
+  {
+    RoleWithSingleSimplestMethod Do();
+  }
 
-	public class VerifyInSequenceFixture
-	{
-		[Fact]
-		public void	ShouldThrowExceptionWhenUsingStaticVerifyShortcutOnMocksWithDifferentSequences()
-		{
-			//GIVEN
-			var	m1 = new Mock<RoleWithSingleSimplestMethod>()	{	CallSequence = new CallSequence(MockBehavior.Loose)	};
-			var	m2 = new Mock<RoleWithSingleSimplestMethod>()	{	CallSequence = new CallSequence(MockBehavior.Loose)	};
+  public interface RoleWithProperty
+  {
+    string Anything { get; set; }
+    string AnythingElse { get; set; }
+  }
 
-			//WHEN
-			m1.Object.Do();
-			m2.Object.Do();
+  public class VerifyInSequenceFixture
+  {
+    [Fact]
+    public void ShouldThrowExceptionWhenUsingStaticVerifyShortcutOnMocksWithDifferentSequences()
+    {
+      //GIVEN
+      var m1 = new Mock<RoleWithSingleSimplestMethod>() { CallSequence = new CallSequence(MockBehavior.Loose) };
+      var m2 = new Mock<RoleWithSingleSimplestMethod>() { CallSequence = new CallSequence(MockBehavior.Loose) };
 
-			//THEN
+      //WHEN
+      m1.Object.Do();
+      m2.Object.Do();
 
-			Assert.Throws<MockException>(()	=> 
-				CallSequence.Verify(
-					m1.CallTo(m	=> m.Do()),
-					m2.CallTo(m	=> m.Do()))
-			);
-		}
-	}
+      //THEN
+
+      Assert.Throws<MockException>(() =>
+        CallSequence.Verify(
+          m1.CallTo(m => m.Do()),
+          m2.CallTo(m => m.Do()))
+      );
+    }
+  }
 }
 
