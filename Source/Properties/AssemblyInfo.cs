0c9d23054ae4c45ebcae62dee0f4462146d5fc16
Jeremy Meng <yumeng@microsoft.com>
2016-04-11 10:35:43 -0700

    Integrate the netcore project to build.
    - Update version to 4.4.
    - Need to use a pre-released version because the Castle.Core dependency is pre-released.

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -9,13 +9,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("Moq")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-#if !NETCORE
-[assembly: AssemblyVersion("4.2.1308.2321")]
-[assembly: AssemblyFileVersion("4.2.1308.2321")]
-#else
-[assembly: AssemblyVersion("4.3.0.0")]
-[assembly: AssemblyFileVersion("4.3.0.0")]
-#endif
+[assembly: AssemblyVersion("4.4.1604.1117")]
+[assembly: AssemblyFileVersion("4.4.1604.1117")]
 
 [assembly: ComVisible(false)]
 [assembly: NeutralResourcesLanguage("en")]
--- build.cmd
+++ build.cmd
@@ -1,1 +1,1 @@
-"C:\Program Files (x86)\MSBuild\12.0\Bin\msbuild.exe" %~dp0\build.proj /verbosity:normal /nr:false
+"C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe" %~dp0\build.proj /verbosity:normal /nr:false
