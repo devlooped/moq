9bc3b834ca6586d744f6119a8645a70c07151ea7
Saro Taşciyan <SaroTasciyan@users.noreply.github.com>
2016-12-10 19:35:05 +0200

    ReturnsAsync() lazy evaluation issue fix #303 (#309)
    * ReturnsAsync() lazy evaluation issue fix #303

--- Source/ReturnsExtensions.cs
+++ Source/ReturnsExtensions.cs
@@ -12,25 +12,37 @@ namespace Moq
     public static class ReturnsExtensions
     {
         /// <summary>
-        /// Allows to specify the return value of an asynchronous method.
+        /// Specifies the value to return from an asynchronous method.
         /// </summary>
+        /// <typeparam name="TMock">Mocked type.</typeparam>
+        /// <typeparam name="TResult">Type of the return value.</typeparam>
+        /// <param name="mock">Returns verb which represents the mocked type and the task of return type</param>
+        /// <param name="value">The value to return, or <see longword="null"/>.</param>
         public static IReturnsResult<TMock> ReturnsAsync<TMock, TResult>(this IReturns<TMock, Task<TResult>> mock, TResult value) where TMock : class
         {
-			return mock.Returns(Task.FromResult(value));
+            return mock.ReturnsAsync(() => value);
         }
 
-		/// <summary>
-		/// Allows to specify the return value of an asynchronous method.
-		/// </summary>
-		public static IReturnsResult<TMock> ReturnsAsync<TMock, TResult>(this IReturns<TMock, Task<TResult>> mock, Func<TResult> value) where TMock : class
+        /// <summary>
+        /// Specifies a function that will calculate the value to return from the asynchronous method.
+        /// </summary>
+        /// <typeparam name="TMock">Mocked type.</typeparam>
+        /// <typeparam name="TResult">Type of the return value.</typeparam>
+        /// <param name="mock">Returns verb which represents the mocked type and the task of return type</param>
+        /// <param name="valueFunction">The function that will calculate the return value.</param>
+        public static IReturnsResult<TMock> ReturnsAsync<TMock, TResult>(this IReturns<TMock, Task<TResult>> mock, Func<TResult> valueFunction) where TMock : class
 		{
-			return mock.ReturnsAsync(value());
+		    return mock.Returns(() => Task.FromResult(valueFunction()));
 		}
 
-		/// <summary>
-		/// Allows to specify the exception thrown by an asynchronous method.
-		/// </summary>
-		public static IReturnsResult<TMock> ThrowsAsync<TMock, TResult>(this IReturns<TMock, Task<TResult>> mock, Exception exception) where TMock : class
+        /// <summary>
+        /// Specifies the exception to throw when the asynchronous method is invoked.
+        /// </summary>
+        /// <typeparam name="TMock">Mocked type.</typeparam>
+        /// <typeparam name="TResult">Type of the return value.</typeparam>
+        /// <param name="mock">Returns verb which represents the mocked type and the task of return type</param>
+        /// <param name="exception">Exception instance to throw.</param>
+        public static IReturnsResult<TMock> ThrowsAsync<TMock, TResult>(this IReturns<TMock, Task<TResult>> mock, Exception exception) where TMock : class
         {
             var tcs = new TaskCompletionSource<TResult>();
             tcs.SetException(exception);
