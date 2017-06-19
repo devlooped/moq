devlooped/moq

commit 162a54379026a42bbc746335d63316819d078b56

Add exiting implemented interfaces to ImplementedInterfaces

Source/Mock.Generic.cs
@@ -112,6 +112,7 @@ namespace Moq
 			this.Behavior = behavior;
 			this.Interceptor = new Interceptor(behavior, typeof(T), this);
 			this.constructorArguments = args;
+            this.ImplementedInterfaces.AddRange(typeof(T).GetInterfaces());
 			this.ImplementedInterfaces.Add(typeof(IMocked<T>));
 
 			this.CheckParameters();

commit 0e3eaf3d7cb2dca83e927fd42510be611ec53548

Fix interface bug introduced by 162a543
Commit 162a543 introduced a bug by handing over too many interfaces
to Castle's `ProxyGenerator.CreateClassProxy` method. The intention
behind that commit apparently was to enable `mock.As<TInterface>`
even after `mock.Object` has already been called.

That change unfortunately also caused mocked class' methods that im-
plement an interface method to no longer be mocked correctly.

This commit fixes this problem by ensuring that the "internally
implemented" interfaces are not explicitly passed to `.CreateClass-
Proxy` (like it used to be done previously).

Source/Mock.Generic.cs
@@ -213,7 +213,7 @@ namespace Moq
 					this.instance = (T)proxyFactory.CreateProxy(
 						typeof(T),
 						this.Interceptor,
-						this.ImplementedInterfaces.ToArray(),
+						this.ImplementedInterfaces.Skip(this.InternallyImplementedInterfaceCount - 1).ToArray(),
 						this.constructorArguments);
 				}
 			});