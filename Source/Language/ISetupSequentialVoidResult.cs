99872e6520340965c57cd04790a356812c555450
Alex Bestul <alexbestul@users.noreply.github.com>
2017-09-21 23:44:59 -0500

    Add support for void sequences

---
+++ Source/Language/ISetupSequentialVoidResult.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Moq.Language
+{
+	/// <summary>
+	/// Language for ReturnSequence
+	/// </summary>
+	public interface ISetupSequentialVoidResult
+	{
+		/// <summary>
+		/// Does nothing
+		/// </summary>
+		ISetupSequentialVoidResult Pass();
+
+		/// <summary>
+		/// Throws an exception
+		/// </summary>
+		ISetupSequentialVoidResult Throws<TException>() where TException : Exception, new();
+
+		/// <summary>
+		/// Throws an exception
+		/// </summary>
+		ISetupSequentialVoidResult Throws(Exception exception);
+	}
+}
