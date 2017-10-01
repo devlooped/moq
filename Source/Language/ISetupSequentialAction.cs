d6d83aaa5c202e6423ae7f5cf5b0c18626cdf1e3
Alex Bestul <alexbestul@users.noreply.github.com>
2017-10-01 16:46:30 -0500

    Add detail to ISetupSequentialAction comments

--- Source/Language/ISetupSequentialAction.cs
+++ Source/Language/ISetupSequentialAction.cs
@@ -3,23 +3,52 @@
 namespace Moq.Language
 {
 	/// <summary>
-	/// Language for ReturnSequence
-	/// </summary>
+	 /// Defines the <c>Pass</c> and <c>Throws</c> verbs for sequence setups 
+	 /// on <c>void</c> methods.
+	 /// </summary>
 	public interface ISetupSequentialAction
 	{
 		/// <summary>
-		/// Does nothing
+		/// Configures the next call in the sequence to do nothing.
 		/// </summary>
+		/// <example>
+		/// The following code configures the first call to <c>Exuecute()</c>
+		/// to do nothing, and the second call to throw an exception.
+		/// <code>
+		/// mock.SetupSequence(m => m.Execute())
+		///    .Pass()
+		///    .Throws&lt;InvalidOperationException&gt;();
+		/// </code>
+		/// </example>
 		ISetupSequentialAction Pass();
 
 		/// <summary>
-		/// Throws an exception
+		/// Configures the next call in the sequence to throw an exception.
 		/// </summary>
-		ISetupSequentialAction Throws<TException>() where TException : Exception, new();
+		/// <example>
+		/// The following code configures the first call to <c>Exuecute()</c>
+		/// to do nothing, and the second call to throw an exception.
+		/// <code>
+		/// mock.SetupSequence(m => m.Execute())
+		///    .Pass()
+		///    .Throws&lt;InvalidOperationException&gt;();
+		/// </code>
+		/// </example>
+		ISetupSequentialAction Throws<TException>() 
+			where TException : Exception, new();
 
 		/// <summary>
-		/// Throws an exception
+		/// Configures the next call in the sequence to throw an exception.
 		/// </summary>
+		/// <example>
+		/// The following code configures the first call to <c>Exuecute()</c>
+		/// to do nothing, and the second call to throw an exception.
+		/// <code>
+		/// mock.SetupSequence(m => m.Execute())
+		///    .Pass()
+		///    .Throws(new InvalidOperationException());
+		/// </code>
+		/// </example>
 		ISetupSequentialAction Throws(Exception exception);
 	}
 }
