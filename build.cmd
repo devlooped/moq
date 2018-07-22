58b4c475e70f31c32a52caa157124bc69e76161b
stakx <stakx@eml.cc>
2018-07-22 16:31:55 +0200

    Use MSBuild-integrated package restore
    This is the final change that makes an external `nuget` tool obsolete,
    which lets us remove a ton of build script stuff.

--- build.cmd
+++ build.cmd
@@ -5,8 +5,6 @@
 SETLOCAL ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
 PUSHD "%~dp0" >NUL
 
-SET CACHED_NUGET=%LocalAppData%\NuGet\NuGet.exe
-
 :: Determine if MSBuild can be located. Allows for a better error message below.
 where msbuild > %TEMP%\msbuild.txt
 set /p msb=<%TEMP%\msbuild.txt
@@ -16,27 +14,14 @@ IF "%msb%"=="" (
     exit /b -1
 )
 
-IF EXIST %CACHED_NUGET% goto copynuget
-echo Downloading latest version of NuGet.exe...
-IF NOT EXIST %LocalAppData%\NuGet md %LocalAppData%\NuGet
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest 'https://dist.nuget.org/win-x86-commandline/latest/nuget.exe' -OutFile '%CACHED_NUGET%'"
-
-:copynuget
-IF EXIST .nuget\nuget.exe goto updateAppVeyorBuildVersion
-md .nuget
-copy %CACHED_NUGET% .nuget\nuget.exe > nul
-.nuget\nuget.exe update -self
-
 :updateAppVeyorBuildVersion
 IF /i +%APPVEYOR%+ NEQ +True+ GOTO :restore
 echo Running "%msb%" build\AppVeyor.proj /r /t:UpdateBuildVersion
 "%msb%" build\AppVeyor.proj /r /t:UpdateBuildVersion
 
 :restore
-:: Build script packages have no version in the path, so we install them to .nuget\packages to avoid conflicts with 
-:: solution/project packages.
-IF NOT EXIST packages.config goto run
-.nuget\NuGet.exe install packages.config -OutputDirectory .nuget\packages -ExcludeVersion
+echo Running "%msb%" Moq.sln /t:Restore
+"%msb%" Moq.sln /t:Restore
 
 :run
 echo Running "%msb%" build.proj /v:normal %1 %2 %3 %4 %5 %6 %7 %8 %9
