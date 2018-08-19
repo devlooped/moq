8281d5b8f9bb858269447e14c3498c2d070a1462
stakx <stakx@eml.cc>
2018-08-20 01:53:43 +0200

    New method for splitting up multi-dot expressions
    This commit adds a new extension method `lambdaExpression.Split()`
    to split up composite expressions such as `a => a.B.C(u).D[v](w) = x`
    into its constituent parts that can be set up one at a time; e.g.:
    
     * a => a.B         property access
     * b => b.C(u)      method calls
     * c => c.D         property access
     * d => d[v]        indexer
     * e => e(w) = x    delegate invocation and assignment
    
    This method will later get used to take apart setup and verification
    expressions and distribute each part across inner mocks. This means
    that expressions will no longer have to be compiled just to generate
    the inner mock graph. This in turn will mean better support for argum-
    ent matchers in such multi-dot ("fluent") expressions.
    
    (This commit also expands `ExpressionComparer` with logic for indexers
    and assignments because this is needed in tests. Moq will likely use
    these kinds of expression tree nodes even though the C# compiler does
    not produce / allow them in expression tree literals.)

--- src/Moq/ExpressionExtensions.cs
+++ src/Moq/ExpressionExtensions.cs
@@ -3,6 +3,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Globalization;
 using System.Linq;
 using System.Linq.Expressions;
@@ -38,30 +39,241 @@ namespace Moq
 		}
 
 		/// <summary>
+		///   Splits an expression such as `<c>m => m.A.B(x).C[y] = z</c>` into a chain of parts
+		///   that can be set up one at a time:
+		///   <list>
+		///     <item>`<c>m => m.A</c>`</item>,
+		///     <item>`<c>... => ....B(x)</c>`</item>,
+		///     <item>`<c>... => ....C</c>`</item>,
+		///     <item>`<c>... => ...[y] = z</c>`</item>.
+		///   </list>
+		///   <para>
+		///     The split points are chosen such that each part has exactly one associated
+		///     <see cref="MethodInfo"/> and optionally some argument expressions.
+		///   </para>
+		/// </summary>
+		/// <exception cref="ArgumentException">
+		///   It was not possible to completely split up the expression.
+		/// </exception>
+		internal static Stack<LambdaExpressionPart> Split(this LambdaExpression expression)
+		{
+			Debug.Assert(expression != null);
+
+			var parts = new Stack<LambdaExpressionPart>();
+
+			Expression remainder = expression.Body;
+			while (CanSplit(remainder))
+			{
+				Split(remainder, out remainder, out var part);
+				parts.Push(part);
+			}
+
+			if (parts.Count > 0 && remainder is ParameterExpression)
+			{
+				return parts;
+			}
+			else
+			{
+				throw new ArgumentException(
+					string.Format(
+						CultureInfo.CurrentCulture,
+						Resources.UnsupportedExpression,
+						remainder.ToStringFixed()));
+			}
+
+			bool CanSplit(Expression e)
+			{
+				switch (e.NodeType)
+				{
+					case ExpressionType.Assign:
+					{
+						var assignmentExpression = (BinaryExpression)e;
+						return CanSplit(assignmentExpression.Left);
+					}
+
+					case ExpressionType.Call:
+					case ExpressionType.Index:
+					{
+						return true;
+					}
+
+					case ExpressionType.Invoke:
+					{
+						var invocationExpression = (InvocationExpression)e;
+						return typeof(Delegate).IsAssignableFrom(invocationExpression.Expression.Type);
+					}
+
+					case ExpressionType.MemberAccess:
+					{
+						var memberAccessExpression = (MemberExpression)e;
+						return memberAccessExpression.Member is PropertyInfo;
+					}
+
+					case ExpressionType.Parameter:
+					default:
+					{
+						return false;
+					}
+				}
+			}
+
+			void Split(Expression e, out Expression r /* remainder */, out LambdaExpressionPart p /* part */)
+			{
+				const string ParameterName = "...";
+
+				switch (e.NodeType)
+				{
+					case ExpressionType.Assign:  // assignment to a property or indexer
+					{
+						var assignmentExpression = (BinaryExpression)e;
+						Split(assignmentExpression.Left, out r, out var lhs);
+						PropertyInfo property;
+						if (lhs.Expression.Body is MemberExpression me)
+						{
+							Debug.Assert(me.Member is PropertyInfo);
+							property = (PropertyInfo)me.Member;
+						}
+						else
+						{
+							Debug.Assert(lhs.Expression.Body is IndexExpression);
+							property = ((IndexExpression)lhs.Expression.Body).Indexer;
+						}
+						var parameter = Expression.Parameter(r.Type, r is ParameterExpression ope ? ope.Name : ParameterName);
+						var arguments = new Expression[lhs.Arguments.Count + 1];
+						for (var ai = 0; ai < arguments.Length - 1; ++ai)
+						{
+							arguments[ai] = lhs.Arguments[ai];
+						}
+						arguments[arguments.Length - 1] = assignmentExpression.Right;
+						p = new LambdaExpressionPart(
+							expression: Expression.Lambda(
+								Expression.Assign(lhs.Expression.Body, assignmentExpression.Right),
+								parameter),
+							method: property.GetSetMethod(true),
+							arguments);
+						return;
+					}
+
+					case ExpressionType.Call:  // regular method call
+					{
+						var methodCallExpression = (MethodCallExpression)e;
+						if (!methodCallExpression.Method.IsStatic)
+						{
+							r = methodCallExpression.Object;
+							var parameter = Expression.Parameter(r.Type, r is ParameterExpression ope ? ope.Name : ParameterName);
+							var method = methodCallExpression.Method;
+							var arguments = methodCallExpression.Arguments;
+							p = new LambdaExpressionPart(
+										expression: Expression.Lambda(
+											Expression.Call(parameter, method, arguments),
+											parameter),
+										method,
+										arguments);
+						}
+						else
+						{
+							Debug.Assert(methodCallExpression.Method.IsExtensionMethod());
+							Debug.Assert(methodCallExpression.Arguments.Count > 0);
+							r = methodCallExpression.Arguments[0];
+							var parameter = Expression.Parameter(r.Type, r is ParameterExpression ope ? ope.Name : ParameterName);
+							var method = methodCallExpression.Method;
+							var arguments = methodCallExpression.Arguments.ToArray();
+							arguments[0] = parameter;
+							p = new LambdaExpressionPart(
+										expression: Expression.Lambda(
+											Expression.Call(method, arguments),
+											parameter),
+										method,
+										arguments);
+						}
+						return;
+					}
+
+					case ExpressionType.Index:  // indexer query
+					{
+						var indexExpression = (IndexExpression)e;
+						r = indexExpression.Object;
+						var parameter = Expression.Parameter(r.Type, r is ParameterExpression ope ? ope.Name : ParameterName);
+						var indexer = indexExpression.Indexer;
+						var arguments = indexExpression.Arguments;
+						p = new LambdaExpressionPart(
+									expression: Expression.Lambda(
+										Expression.MakeIndex(parameter, indexer, arguments),
+										parameter),
+									method: indexer.GetGetMethod(true),
+									arguments);
+						return;
+					}
+
+					case ExpressionType.Invoke:  // delegate invocation
+					{
+						var invocationExpression = (InvocationExpression)e;
+						Debug.Assert(invocationExpression.Expression.Type.IsDelegate());
+						r = invocationExpression.Expression;
+						var parameter = Expression.Parameter(r.Type, r is ParameterExpression ope ? ope.Name : ParameterName);
+						var arguments = invocationExpression.Arguments;
+						p = new LambdaExpressionPart(
+									expression: Expression.Lambda(
+										Expression.Invoke(parameter, arguments),
+										parameter),
+									method: r.Type.GetMethod("Invoke", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance),
+									arguments);
+						return;
+					}
+
+					case ExpressionType.MemberAccess:  // property query
+					{
+						var memberAccessExpression = (MemberExpression)e;
+						Debug.Assert(memberAccessExpression.Member is PropertyInfo);
+						r = memberAccessExpression.Expression;
+						var parameter = Expression.Parameter(r.Type, r is ParameterExpression ope ? ope.Name : ParameterName);
+						var property = memberAccessExpression.GetReboundProperty();
+						p = new LambdaExpressionPart(
+									expression: Expression.Lambda(
+										Expression.MakeMemberAccess(parameter, property),
+										parameter),
+									method: property.GetGetMethod(true));
+						return;
+					}
+
+					default:
+						Debug.Assert(!CanSplit(e));
+						throw new InvalidOperationException();  // this should be unreachable
+				}
+			}
+		}
+
+		internal static PropertyInfo GetReboundProperty(this MemberExpression expression)
+		{
+			Debug.Assert(expression.Member is PropertyInfo);
+
+			var property = (PropertyInfo)expression.Member;
+
+			// the following block is required because .NET compilers put the wrong PropertyInfo into MemberExpression
+			// for properties originally declared in base classes; they will put the base class' PropertyInfo into
+			// the expression. we attempt to correct this here by checking whether the type of the accessed object
+			// has a property by the same name whose base definition equals the property in the expression; if so,
+			// we "upgrade" to the derived property.
+			if (property.DeclaringType != expression.Expression.Type)
+			{
+				var derivedProperty = expression.Expression.Type.GetProperty(property.Name, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+				if (derivedProperty != null && derivedProperty.GetMethod.GetBaseDefinition() == property.GetMethod)
+				{
+					return derivedProperty;
+				}
+			}
+
+			return property;
+		}
+
+		/// <summary>
 		/// Converts the body of the lambda expression into the <see cref="PropertyInfo"/> referenced by it.
 		/// </summary>
 		public static PropertyInfo ToPropertyInfo(this LambdaExpression expression)
 		{
 			if (expression.Body is MemberExpression prop)
 			{
-				if (prop.Member is PropertyInfo info)
-				{
-					// the following block is required because .NET compilers put the wrong PropertyInfo into MemberExpression
-					// for properties originally declared in base classes; they will put the base class' PropertyInfo into
-					// the expression. we attempt to correct this here by checking whether the type of the accessed object
-					// has a property by the same name whose base definition equals the property in the expression; if so,
-					// we "upgrade" to the derived property.
-					if (info.DeclaringType != prop.Expression.Type && info.CanRead)
-					{
-						var propertyInLeft = prop.Expression.Type.GetProperty(info.Name, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
-						if (propertyInLeft != null && propertyInLeft.GetMethod.GetBaseDefinition() == info.GetMethod)
-						{
-							info = propertyInLeft;
-						}
-					}
-
-					return info;
-				}
+				return prop.GetReboundProperty();
 			}
 
 			throw new ArgumentException(string.Format(
