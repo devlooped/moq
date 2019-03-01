7ec11c96f93ad28601bc56519ebf945da2c189c0
stakx <stakx@eml.cc>
2019-03-01 16:24:33 +0100

    Document `Object` methods interceptability bug
    The failing tests added in this commit demonstrate that `ProxyFactory`
    doesn't currently guarantee interceptability of `Object` methods under
    all circumstances.
    
    More precisely, `Object` methods are currently not interceptable if:
    
     * the proxied type is an interface type, and
     * the interceptor used is not an instance of the `Mock` class.

---
+++ tests/Moq.Tests/InterceptorFixture.cs
@@ -0,0 +1,72 @@
+// Copyright (c) 2007, Clarius Consulting, Manas Technology Solutions, InSTEDD.
+// All rights reserved. Licensed under the BSD 3-Clause License; see License.txt.
+
+using System;
+
+using Xunit;
+
+namespace Moq.Tests
+{
+	public class InterceptorFixture
+	{
+		/// <summary>
+		///   These tests document which methods can be intercepted (i.e. seen) by <see cref="IInterceptor"/>s.
+		/// </summary>
+		public class Method_interceptability
+		{
+			[Theory]
+			[InlineData(typeof(ClassType))]
+			[InlineData(typeof(IInterfaceType))]
+			public void Object_Equals(Type typeToProxy)
+			{
+				var proxy = CreateProxy(typeToProxy, new Echo(true));
+				var returnValue = proxy.Equals(42);
+				Assert.True(returnValue);
+			}
+
+			[Theory]
+			[InlineData(typeof(ClassType))]
+			[InlineData(typeof(IInterfaceType))]
+			public void Object_GetHashCode(Type typeToProxy)
+			{
+				var proxy = CreateProxy(typeToProxy, new Echo(42));
+				var returnValue = proxy.GetHashCode();
+				Assert.Equal(42, returnValue);
+			}
+
+			[Theory]
+			[InlineData(typeof(ClassType))]
+			[InlineData(typeof(IInterfaceType))]
+			public void Object_ToString(Type typeToProxy)
+			{
+				var proxy = CreateProxy(typeToProxy, new Echo("42"));
+				var returnValue = proxy.ToString();
+				Assert.Equal("42", returnValue);
+			}
+
+			public abstract class ClassType { }
+
+			public interface IInterfaceType { }
+		}
+
+		private static object CreateProxy(Type type, IInterceptor interceptor)
+		{
+			return ProxyFactory.Instance.CreateProxy(type, interceptor, Type.EmptyTypes, new object[0]);
+		}
+
+		private sealed class Echo : IInterceptor
+		{
+			private readonly object returnValue;
+
+			public Echo(object returnValue)
+			{
+				this.returnValue = returnValue;
+			}
+
+			public void Intercept(Invocation invocation)
+			{
+				invocation.Return(this.returnValue);
+			}
+		}
+	}
+}
