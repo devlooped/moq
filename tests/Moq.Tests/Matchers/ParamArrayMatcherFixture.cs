ac4520b51016f9360b643f4b179de274c37863bc
stakx <stakx@eml.cc>
2019-08-29 23:25:31 +0200

    `Capture.In` does not work in `params` parameter
    If `canCastValueToT` check added in the previous commit is extracted
    and reused in `Match.SetupEvaluatedSuccessfully` (where the same type
    of cast happens) as an assertion, that assertion will trigger. There's
    a bug in `ParamArrayMatcher`.

---
+++ tests/Moq.Tests/Matchers/ParamArrayMatcherFixture.cs
@@ -0,0 +1,54 @@
+// Copyright (c) 2007, Clarius Consulting, Manas Technology Solutions, InSTEDD.
+// All rights reserved. Licensed under the BSD 3-Clause License; see License.txt.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using Xunit;
+
+namespace Moq.Tests.Matchers
+{
+	public class ParamArrayMatcherFixture
+	{
+		[Theory]
+		[InlineData(42, "", true)]
+		[InlineData(42, null, true)]
+		[InlineData(3.141f, "", false)]
+		[InlineData(null, "", false)]
+		public void Matches_several_matchers_from_params_array(object first, object second, bool shouldMatch)
+		{
+			var seconds = new List<string>();
+			var methodCallExpr = (MethodCallExpression)ToExpression<IX>(x => x.Method(It.IsAny<int>(), Capture.In(seconds))).Body;
+			var expr = methodCallExpr.Arguments.Single();
+			var parameter = typeof(IX).GetMethod("Method").GetParameters().Single();
+
+			var (matcher, _) = MatcherFactory.CreateMatcher(expr, parameter);
+
+			Assert.Equal(shouldMatch, matcher.Matches(new object[] { first, second }));
+		}
+
+		[Fact]
+		public void SetupEvaluatedSuccessfully_succeeds_for_matching_values()
+		{
+			var seconds = new List<string>();
+			var methodCallExpr = (MethodCallExpression)ToExpression<IX>(x => x.Method(It.IsAny<int>(), Capture.In(seconds))).Body;
+			var expr = methodCallExpr.Arguments.Single();
+			var parameter = typeof(IX).GetMethod("Method").GetParameters().Single();
+
+			var (matcher, _) = MatcherFactory.CreateMatcher(expr, parameter);
+
+			matcher.SetupEvaluatedSuccessfully(new object[] { 42, "" });
+		}
+
+		private LambdaExpression ToExpression<T>(Expression<Action<T>> expr)
+		{
+			return expr;
+		}
+
+		public interface IX
+		{
+			void Method(params object[] args);
+		}
+	}
+}
