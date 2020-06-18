dd46d30677c5f9fb48bc69d769f718403cc5d9af
stakx <stakx@eml.cc>
2020-06-18 22:38:06 +0200

    Remove unneeded conditional compilation

--- tests/Moq.Tests/ComCompatibilityFixture.cs
+++ tests/Moq.Tests/ComCompatibilityFixture.cs
@@ -1,8 +1,6 @@
 // Copyright (c) 2007, Clarius Consulting, Manas Technology Solutions, InSTEDD.
 // All rights reserved. Licensed under the BSD 3-Clause License; see License.txt.
 
-#if FEATURE_COM
-
 using Moq.Tests.ComTypes;
 
 using Xunit;
@@ -104,5 +102,3 @@ namespace Moq.Tests
 		}
 	}
 }
-
-#endif
