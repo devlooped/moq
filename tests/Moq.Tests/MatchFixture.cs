b62494b9095d22cd85ae4a955d709f7ed3a56995
stakx <stakx@eml.cc>
2020-06-27 14:25:24 +0200

    Include contributors in copyright notice
    Approved by the project owner (@kzu) in:
    https://gitter.im/moq/moq?at=5e9d5872d65bcf75b5d773ad

--- tests/Moq.Tests/MatchFixture.cs
+++ tests/Moq.Tests/MatchFixture.cs
@@ -1,4 +1,4 @@
-// Copyright (c) 2007, Clarius Consulting, Manas Technology Solutions, InSTEDD.
+// Copyright (c) 2007, Clarius Consulting, Manas Technology Solutions, InSTEDD, and Contributors.
 // All rights reserved. Licensed under the BSD 3-Clause License; see License.txt.
 
 using Moq.Protected;
