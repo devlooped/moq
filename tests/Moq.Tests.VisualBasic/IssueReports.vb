3134e1c182378c4185764f91b03b79a580688c04
stakx <stakx@eml.cc>
2020-10-11 12:47:01 +0200

    Add failing test re: VB.NET `Convert` nodes
    It appears that VB.NET adds a pair of `Convert` nodes to expression
    trees for constrained generic type parameters. Those double conversions
    can end up "hiding" matchers thus wrapped from Moq's `MatcherFactory`.

--- tests/Moq.Tests.VisualBasic/IssueReports.vb
+++ tests/Moq.Tests.VisualBasic/IssueReports.vb
@@ -1,10 +1,9 @@
 ' Copyright (c) 2007, Clarius Consulting, Manas Technology Solutions, InSTEDD, and Contributors.
 ' All rights reserved. Licensed under the BSD 3-Clause License; see License.txt.
 
-Imports Moq
+Imports System.Linq.Expressions
+
 Imports Xunit
-
-
 
 Public Class IssueReports
 
@@ -27,6 +26,53 @@ Public Class IssueReports
 			ReadOnly Property PropertyWithMultipleArgs(setting As Integer, setting2 As Integer) As Integer
 
 		End Interface
+	End Class
+
+	Public Class Issue1067
+
+		<Fact>
+		Public Sub Test_NonGeneric()
+			Dim userManagerMock = New Mock(Of IUserManager)()
+			Setup_NonGeneric(userManagerMock, 42)
+
+			Dim user As New User()
+			userManagerMock.Object.Create(user)
+
+			Assert.Equal(42, user.Id)
+		End Sub
+
+		<Fact>
+		Public Sub Test_Generic()
+			Dim userManagerMock = New Mock(Of IUserManager)()
+			Setup_Generic(Of User)(userManagerMock, 42)
+
+			Dim user As New User()
+			userManagerMock.Object.Create(user)
+
+			Assert.Equal(42, user.Id)
+		End Sub
+
+		Public Class User
+			Property Id As Integer
+		End Class
+
+		Public Interface IUserManager
+			Sub Create(User As User)
+		End Interface
+
+		Protected Sub Setup_NonGeneric(userManagerMock As Mock(Of IUserManager), expectedId As Integer)
+			userManagerMock.Setup(Sub(manager) manager.Create(It.IsAny(Of User))).Callback(Sub(user) user.Id = expectedId)
+		End Sub
+
+		Protected Sub Setup_Generic(Of TUser As User)(userManagerMock As Mock(Of IUserManager), expectedId As Integer)
+			userManagerMock.Setup(Sub(manager) manager.Create(It.IsAny(Of TUser))).Callback(Sub(user) user.Id = expectedId)
+			'                                                             ^
+			' The use of generics will cause the VB.NET compiler to wrap the `It.IsAny<>` call with two `Convert` nodes.
+			' The inner conversion will convert to `Object`, and the outer conversion will convert to `User` (i.e. the type that
+			' `TUser` is constrained to). `MatcherFactory` needs to be able to recognize the `It.IsAny<>` matcher even if it
+			' is doubly wrapped!
+		End Sub
+
 	End Class
 
 End Class
