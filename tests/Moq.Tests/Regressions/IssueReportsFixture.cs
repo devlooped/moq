devlooped/moq

commit cb6b71a60df08a9c62b44ad6a33b608e4c0e11a1

Convert property setups to new helper, too
For this to work, `Mocks.SetupReturns` needs to be extended so it will
allow setting non-interceptable properties. (Accessing such a property
cannot be represented using `InvocationShape`, which guards against
non-interceptable members, so they must be removed from setup expr's
and set manually.)

These changes conclude the conversion of LINQ to Mocks from `Fluent-
MockVisitor` to the infrastructure provided by `Mock.SetupRecursive`
and `expression.Split()`, which throws different exceptions when en-
countering unsupported members. Adjust some tests to reflect this.

src/Moq/ExpressionExtensions.cs
@@ -79,6 +79,47 @@ namespace Moq
 		}
 
 		/// <summary>
+		///   Checks whether the given expression <paramref name="e"/> can be split by <see cref="Split"/>.
+		/// </summary>
+		public static bool CanSplit(this Expression e)
+		{
+			switch (e.NodeType)
+			{
+				case ExpressionType.Assign:
+				case ExpressionType.AddAssign:
+				case ExpressionType.SubtractAssign:
+				{
+					var assignmentExpression = (BinaryExpression)e;
+					return CanSplit(assignmentExpression.Left);
+				}
+
+				case ExpressionType.Call:
+				case ExpressionType.Index:
+				{
+					return true;
+				}
+
+				case ExpressionType.Invoke:
+				{
+					var invocationExpression = (InvocationExpression)e;
+					return typeof(Delegate).IsAssignableFrom(invocationExpression.Expression.Type);
+				}
+
+				case ExpressionType.MemberAccess:
+				{
+					var memberAccessExpression = (MemberExpression)e;
+					return memberAccessExpression.Member is PropertyInfo;
+				}
+
+				case ExpressionType.Parameter:
+				default:
+				{
+					return false;
+				}
+			}
+		}
+
+		/// <summary>
 		///   Splits an expression such as `<c>m => m.A.B(x).C[y] = z</c>` into a chain of parts
 		///   that can be set up one at a time:
 		///   <list>
@@ -119,44 +160,6 @@ namespace Moq
 						CultureInfo.CurrentCulture,
 						Resources.UnsupportedExpression,
 						remainder.ToStringFixed()));
-			}
-
-			bool CanSplit(Expression e)
-			{
-				switch (e.NodeType)
-				{
-					case ExpressionType.Assign:
-					case ExpressionType.AddAssign:
-					case ExpressionType.SubtractAssign:
-					{
-						var assignmentExpression = (BinaryExpression)e;
-						return CanSplit(assignmentExpression.Left);
-					}
-
-					case ExpressionType.Call:
-					case ExpressionType.Index:
-					{
-						return true;
-					}
-
-					case ExpressionType.Invoke:
-					{
-						var invocationExpression = (InvocationExpression)e;
-						return typeof(Delegate).IsAssignableFrom(invocationExpression.Expression.Type);
-					}
-
-					case ExpressionType.MemberAccess:
-					{
-						var memberAccessExpression = (MemberExpression)e;
-						return memberAccessExpression.Member is PropertyInfo;
-					}
-
-					case ExpressionType.Parameter:
-					default:
-					{
-						return false;
-					}
-				}
 			}
 
 			void Split(Expression e, out Expression r /* remainder */, out InvocationShape p /* part */)

src/Moq/Linq/MockSetupsBuilder.cs
@@ -116,33 +116,10 @@ namespace Moq.Linq
 		{
 			switch (left.NodeType)
 			{
-				case ExpressionType.MemberAccess:
-					var member = (MemberExpression)left;
-					Guard.NotField(member);
-
-					return ConvertToSetupProperty(member.Expression, member, right);
-
 				case ExpressionType.Call:
-					var method = (MethodCallExpression)left;
-
-					if (!method.Method.CanOverride())
-						throw new NotSupportedException(string.Format(
-							CultureInfo.CurrentCulture,
-							Resources.LinqMethodNotVirtual,
-							method.ToStringFixed()));
-
-					return ConvertToSetupReturns(method, right);
-
 				case ExpressionType.Invoke:
-					var invocation = (InvocationExpression)left;
-					if (invocation.Expression is ParameterExpression && typeof(Delegate).IsAssignableFrom(invocation.Expression.Type))
-					{
-						return ConvertToSetupReturns(invocation, right);
-					}
-					else
-					{
-						break;
-					}
+				case ExpressionType.MemberAccess:
+					return ConvertToSetupReturns(left, right);
 
 				case ExpressionType.Convert:
 					var left1 = (UnaryExpression)left;
@@ -150,45 +127,6 @@ namespace Moq.Linq
 			}
 
 			return null;
-		}
-
-		private static Expression ConvertToSetupProperty(Expression targetObject, Expression left, Expression right)
-		{
-			// TODO: throw if target is a static class?
-			var sourceType = targetObject.Type;
-			var propertyInfo = (PropertyInfo)((MemberExpression)left).Member;
-			var propertyType = propertyInfo.PropertyType;
-
-			// where foo.Name == "bar"
-			// becomes:	
-			// where Mocks.SetProperty(Mock.Get(foo), mock => mock.Name, (object)"bar")
-
-			// if the property is readonly, we can only do a Setup(...) which is the same as a method setup.
-			if (!propertyInfo.CanWrite(out var setter) || setter.IsPrivate)
-				return ConvertToSetupReturns(left, right);
-
-			// This will get up to and including the Mock.Get(foo).Setup(mock => mock.Name) call.
-			var propertySetup = VisitFluent(left);
-			// We need to go back one level, to the target expression of the Setup call, 
-			// which would be the Mock.Get(foo), where we will actually invoke SetupProperty instead.
-			if (propertySetup.NodeType != ExpressionType.Call)
-				throw new NotSupportedException(string.Format(Resources.UnexpectedTranslationOfMemberAccess, propertySetup.ToStringFixed()));
-
-			var propertyCall = (MethodCallExpression)propertySetup;
-			var mockExpression = propertyCall.Object;
-			var propertyExpression = propertyCall.Arguments.First().StripQuotes();
-
-			// We can safely just set the value here since `SetProperty` will temporarily enable auto-stubbing
-			// if the underlying `IQueryable` provider implementation hasn't already enabled it permanently by
-			// calling `SetupAllProperties`.
-			//
-			// This method also enables the use of this querying capability against plain DTO even
-			// if their properties are not virtual.
-			return Expression.Call(
-				Mocks.SetPropertyMethod,
-				mockExpression,
-				propertyCall.Arguments.First(),
-				Expression.Convert(right, typeof(object)));  // explicit boxing operation required for value types
 		}
 
 		/// <summary>

src/Moq/Linq/Mocks.cs
@@ -128,18 +128,12 @@ namespace Moq
 			while (true);
 		}
 
-		internal static readonly MethodInfo SetPropertyMethod =
-			typeof(Mocks).GetMethod(nameof(SetProperty), BindingFlags.NonPublic | BindingFlags.Static);
-
 		/// <summary>
 		/// Extension method used to support Linq-like setup properties that are not virtual but do have 
 		/// a getter and a setter, thereby allowing the use of Linq to Mocks to quickly initialize DTOs too :)
 		/// </summary>
-		internal static bool SetProperty(Mock target, LambdaExpression propertyReference, object value)
+		private static bool SetProperty(Mock target, PropertyInfo property, object value)
 		{
-			var memberExpr = (MemberExpression)propertyReference.Body;
-			var member = (PropertyInfo)memberExpr.Member;
-
 			// For strict mocks, we haven't called `SetupAllProperties` on the mock being set up.
 			// Therefore, whenever a property is being initialized, we quickly need to enable auto-stubbing.
 			//
@@ -155,7 +149,7 @@ namespace Moq
 			}
 			try
 			{
-				member.SetValue(target.Object, value, null);
+				property.SetValue(target.Object, value, null);
 			}
 			finally
 			{
@@ -173,8 +167,40 @@ namespace Moq
 
 		internal static bool SetupReturns(Mock mock, LambdaExpression expression, object value)
 		{
+			PropertyInfo propertyToSet = null;
+
+			if (expression.Body is MemberExpression me
+				&& me.Member is PropertyInfo pi
+				&& !(pi.CanRead(out var getter) && getter.CanOverride() && ProxyFactory.Instance.IsMethodVisible(getter, out _))
+				&& pi.CanWrite(out _))
+			{
+				// LINQ to Mocks allows setting non-interceptable properties, which is handy e.g. when initializing DTOs.
+				// Generally, we prefer having a setup for a property's return value, but if that isn't possible (e.g.
+				// with a non-virtual or sealed property), we will have to fall back to simply setting that property
+				// through reflection.
+				//
+				// The above condition detects exactly those cases.
+				propertyToSet = pi;
+
+				// The property access (which is one that cannot be setup) must be subtracted from the setup expression:
+				expression = Expression.Lambda(me.Expression, expression.Parameters);
+				if (!expression.CanSplit())
+				{
+					Mocks.SetProperty(mock, propertyToSet, value);
+					return true;
+				}
+			}
+
 			var setup = Mock.Setup(mock, expression, condition: null);
-			setup.SetEagerReturnsResponse(value);
+
+			if (propertyToSet == null)
+			{
+				setup.SetEagerReturnsResponse(value);
+			}
+			else
+			{
+				Mocks.SetProperty(setup.Mock, propertyToSet, value);
+			}
 
 			return true;
 		}

tests/Moq.Tests/Linq/UnsupportedQuerying.cs
@@ -46,21 +46,21 @@ namespace Moq.Tests.Linq
 		public class GivenAField
 		{
 			[Fact]
-			public void WhenQueryingField_ThenThrowsNotSupportedException()
+			public void WhenQueryingField_ThenThrowsArgumentException()
 			{
-				Assert.Throws<NotSupportedException>(() => Mock.Of<Bar>(x => x.FieldValue == "bar"));
+				Assert.Throws<ArgumentException>(() => Mock.Of<Bar>(x => x.FieldValue == "bar"));
 			}
 
 			[Fact]
-			public void WhenQueryingOnFluent_ThenThrowsNotSupportedException()
+			public void WhenQueryingOnFluent_ThenThrowsArgumentException()
 			{
-				Assert.Throws<NotSupportedException>(() => Mock.Of<Foo>(x => x.VirtualBar.FieldValue == "bar"));
+				Assert.Throws<ArgumentException>(() => Mock.Of<Foo>(x => x.VirtualBar.FieldValue == "bar"));
 			}
 
 			[Fact]
-			public void WhenIntermediateFluentReadonly_ThenThrowsNotSupportedException()
+			public void WhenIntermediateFluentReadonly_ThenThrowsArgumentException()
 			{
-				Assert.Throws<NotSupportedException>(() => Mock.Of<Foo>(x => x.Bar.VirtualValue == "bar"));
+				Assert.Throws<ArgumentException>(() => Mock.Of<Foo>(x => x.Bar.VirtualValue == "bar"));
 			}
 
 			public class Bar

commit 15dd8e9ca66a66470414175f1194575a4256fe89

Add failing test caused by regression in cb6b71a6
Since that commit, inner mocks created by `Mock.Of<>()` no longer have
their properties stubbed. `SetupAllProperties` is now only called for
the root (outermost) mock. That's why nested properties can no longer
be overridden simply by assigning to them.

tests/Moq.Tests/Regressions/IssueReportsFixture.cs
@@ -3451,6 +3451,43 @@ namespace Moq.Tests.Regressions
 
 		#endregion
 
+		#region 1071
+
+		public class Issue1071
+		{
+			public interface IFoo
+			{
+				int Property { get; set; }
+			}
+
+			public interface IBar
+			{
+				IFoo Foo { get; set; }
+			}
+
+			[Fact]
+			public void Property_set_up_with_Mock_Of__is_automatically_stubbed__automatic_inner_mock()
+			{
+				var bar = Mock.Of<IBar>(x => x.Foo.Property == 123);
+				Assert.Equal(123, bar.Foo.Property);
+
+				bar.Foo.Property = 321;
+				Assert.Equal(321, bar.Foo.Property);
+			}
+
+			[Fact]
+			public void Property_set_up_with_Mock_Of_is_automatically_stubbed__manual_inner_mock()
+			{
+				var bar = Mock.Of<IBar>(x => x.Foo == Mock.Of<IFoo>(y => y.Property == 123));
+				Assert.Equal(123, bar.Foo.Property);
+
+				bar.Foo.Property = 321;
+				Assert.Equal(321, bar.Foo.Property);
+			}
+		}
+
+		#endregion
+
 		// Old @ Google Code
 
 		#region #47