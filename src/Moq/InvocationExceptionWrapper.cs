2a6a5f49d46506def2e71e1dfa84c634beb656bc
Martin Strecker <Martin.Strecker@ifpconsulting.de>
2020-10-14 16:13:14 +0100

    Introduce InvocationExceptionWrapper and add logic to IINvocation.ReturnValue and IInvocation.Exception.

---
+++ src/Moq/InvocationExceptionWrapper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moq
+{
+	/// <summary>
+	/// Internal type to mark invocation results as "exception occurred during execution". The type just
+	/// wraps the Exception so a thrown exception can be distinguished from an <see cref="System.Exception"/>
+	/// return by the invocation.
+	/// </summary>
+	internal readonly struct InvocationExceptionWrapper
+	{
+		public InvocationExceptionWrapper(Exception exception)
+		{
+			Exception = exception;
+		}
+
+		public Exception Exception { get; }
+	}
+}
