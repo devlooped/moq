a337baaf152c0a26e60ae659ac6d9f77c70997bc
Dominique Schuppli <stakx@eml.cc>
2022-08-02 19:32:58 +0200

    Add failing test for mocking derived record type

---
+++ tests/Moq.Tests/RecordsFixture.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) 2007, Clarius Consulting, Manas Technology Solutions, InSTEDD, and Contributors.
+// All rights reserved. Licensed under the BSD 3-Clause License; see License.txt.
+
+using Xunit;
+
+namespace Moq.Tests
+{
+	public class RecordsFixture
+	{
+		[Fact]
+		public void Can_mock_EmptyRecord()
+		{
+			_ = new Mock<EmptyRecord>().Object;
+		}
+
+		[Fact]
+		public void Can_mock_DerivedEmptyRecord()
+		{
+			_ = new Mock<DerivedEmptyRecord>().Object;
+		}
+
+		public record EmptyRecord
+		{
+		}
+
+		public record DerivedEmptyRecord : EmptyRecord
+		{
+		}
+	}
+}
