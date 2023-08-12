85c7ec038ea1fdf1f1fe5be04c7a995202791f66
Hassan Habib <hassanhabib@live.com>
2023-08-12 12:07:06 -0700

    CODE RUB: Remove Analysis Project VB and F#

--- src/Moq.Tests.FSharpTypes/IHasIndexer.fs
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2007, Clarius Consulting, Manas Technology Solutions, InSTEDD, and Contributors.
-// All rights reserved. Licensed under the BSD 3-Clause License; see License.txt.
-
-namespace Moq.Tests.FSharpTypes
-
-open System;
-
-type IHasIndexer =
-    abstract member Item: int -> obj with get, set
